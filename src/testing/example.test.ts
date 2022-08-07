6faae6bbbd572d91e084d3827e680056085bafc7
anton <antoine.lyamin@gmail.com>
2022-08-07 14:59:27 +1000

    fix: added necessary pacakges and file to provide a simple unit test example

---
+++ src/testing/example.test.ts
@@ -0,0 +1,17 @@
+import Flight from "../domain/Flight";
+
+// quick example of how to test out a class
+test("Create Flight and use getter for ID", () => {
+  const flight: Flight = new Flight(
+    1,
+    new Date(),
+    "11:00am",
+    2,
+    3,
+    77,
+    747,
+    10,
+    100
+  );
+  expect(flight.getFlightCode()).toBe(1);
+});
