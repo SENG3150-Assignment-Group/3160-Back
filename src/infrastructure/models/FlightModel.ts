1c5f524c4bcfa675a6d7438b9f870b4a21f62398
GeorgeDavis590 <84362469+GeorgeDavis590@users.noreply.github.com>
2022-08-09 14:49:27 +1000

    Updated Flight Model

--- src/infrastructure/models/FlightModel.ts
+++ src/infrastructure/models/FlightModel.ts
@@ -1,192 +1,79 @@
-class FlightModel {
-  aircraft: string;
-  departure: string;
-  destination: string;
-  date: string;
-  takeoffTime: string;
-  landingTime: string;
-  price: number;
-  airline: string;
-  popularity: number;
-  seatClass: string;
-  flightID: string;
-  numberOfSeats: number;
-  seatsBooked: number;
+/* const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 
-  constructor(
-    aircraft: string,
-    departure: string,
-    destination: string,
-    date: string,
-    takeoffTime: string,
-    landingTime: string,
-    price: number,
-    airline: string,
-    popularity: number,
-    seatClass: string, //class
-    flightID: string,
-    numberOfSeats: number,
-    seatsBooked: number
-  ) {
-    this.aircraft = aircraft;
-    this.departure = departure;
-    this.destination = destination;
-    this.date = date;
-    this.takeoffTime = takeoffTime;
-    this.landingTime = landingTime;
-    this.price = price;
-    this.airline = airline;
-    this.popularity = popularity;
-    this.seatClass = seatClass;
-    this.flightID = flightID;
-    this.numberOfSeats = numberOfSeats;
-    this.seatsBooked = seatsBooked;
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
   }
+}, {
+  // Other model options go here
+});
 
-  /*
-   * Static factory method
-   */
-  public static createFlightModel = (
-    aircraft: string,
-    departure: string,
-    destination: string,
-    date: string,
-    takeoffTime: string,
-    landingTime: string,
-    price: number,
-    airline: string,
-    popularity: number,
-    seatClass: string, //class
-    flightID: string,
-    numberOfSeats: number,
-    seatsBooked: number
-  ): FlightModel => {
-    return new FlightModel(
-      aircraft,
-      departure,
-      destination,
-      date,
-      takeoffTime,
-      landingTime,
-      price,
-      airline,
-      popularity,
-      seatClass,
-      flightID,
-      numberOfSeats,
-      seatsBooked
-    );
-  };
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true */
 
-  /*
-   * Getters
-   */
-  public getAircraft = (): string => {
-    return this.aircraft;
-  };
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 
-  public getDeparture = (): string => {
-    return this.departure;
-  };
+const User = sequelize.define('CategoryModel', {
+  // Model attributes are defined here
+  flightCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  departureCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  destinationCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  departureId: {
+    type: DataTypes.NUMBER,
+    allowNull: false
+  },
+  destinationId: {
+    type: DataTypes.NUMBER,
+    allowNull: false
+  },
+  departureDateTime: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  destinationDateTime: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  stopOverCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  airlineCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  planeCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  duration: {
+    type: DataTypes.NUMBER,
+    allowNull: false
+  },
+  numSeats: {
+    type: DataTypes.NUMBER,
+    allowNull: false
+  }
+}, {
+  // Other model options go here
+});
 
-  public getDestination = (): string => {
-    return this.destination;
-  };
-
-  public getDate = (): string => {
-    return this.date;
-  };
-
-  public getTakeoffTime = (): string => {
-    return this.takeoffTime;
-  };
-
-  public getLandingTime = (): string => {
-    return this.landingTime;
-  };
-
-  public getPrice = (): number => {
-    return this.price;
-  };
-
-  public getAirline = (): string => {
-    return this.airline;
-  };
-
-  public getPopularity = (): number => {
-    return this.popularity;
-  };
-
-  public getFlightID = (): string => {
-    return this.flightID;
-  };
-
-  public getSeatClass = (): string => {
-    return this.seatClass;
-  };
-
-  public getNumberOfSeats = (): number => {
-    return this.numberOfSeats;
-  };
-
-  public getSeatsBooked = (): number => {
-    return this.seatsBooked;
-  };
-
-  /*
-   * Setters
-   */
-  public setAircraft = (aircraft: string) => {
-    this.aircraft = aircraft;
-  };
-
-  public setDeparture = (departure: string) => {
-    this.departure = departure;
-  };
-
-  public setDestination = (destination: string) => {
-    this.destination = destination;
-  };
-
-  public setDate = (date: string) => {
-    this.date = date;
-  };
-
-  public setTakeoffTime = (takeoffTime: string) => {
-    this.takeoffTime = takeoffTime;
-  };
-
-  public setLandingTime = (landingTime: string) => {
-    this.landingTime = landingTime;
-  };
-
-  public setPrice = (price: number) => {
-    this.price = price;
-  };
-
-  public setAirline = (airline: string) => {
-    this.airline = airline;
-  };
-
-  public setPopularity = (popularity: number) => {
-    this.popularity = popularity;
-  };
-
-  public setFlightID = (flightID: string) => {
-    this.flightID = flightID;
-  };
-
-  public setSeatClass = (seatClass: string) => {
-    this.seatClass = seatClass;
-  };
-
-  public setNumberOfSeats = (numberOfSeats: number) => {
-    this.numberOfSeats = numberOfSeats;
-  };
-
-  public setSeatsBooked = (seatsBooked: number) => {
-    this.seatsBooked = seatsBooked;
-  };
-}
-
-export default FlightModel;
+// `sequelize.define` also returns the model
+console.log(CategoryModel === sequelize.models.CategoryModel); // true
