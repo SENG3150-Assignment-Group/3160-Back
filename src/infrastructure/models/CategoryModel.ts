9f95ee69c2a49b6cf8b658d8886d4ca0704fa998
GeorgeDavis590 <84362469+GeorgeDavis590@users.noreply.github.com>
2022-08-12 23:37:03 +1000

    changed User to Model name

--- src/infrastructure/models/CategoryModel.ts
+++ src/infrastructure/models/CategoryModel.ts
@@ -21,7 +21,7 @@ console.log(User === sequelize.models.User); // true */
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
 
-const User = sequelize.define('CategoryModel', {
+const CategoryModel = sequelize.define('CategoryModel', {
   // Model attributes are defined here
   categoryId: {
     type: DataTypes.INTEGER,
