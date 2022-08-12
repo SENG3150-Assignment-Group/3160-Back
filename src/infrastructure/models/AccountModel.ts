e01891013c4407cdeeb9458e7aefe29c7d1aa0a2
GeorgeDavis590 <84362469+GeorgeDavis590@users.noreply.github.com>
2022-08-12 23:04:31 +1000

    added Account Model

---
+++ src/infrastructure/models/AccountModel.ts
@@ -0,0 +1,51 @@
+/* const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  }
+}, {
+  // Other model options go here
+});
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true */
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const User = sequelize.define('AccountModel', {
+  // Model attributes are defined here
+  accountId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  // Other model options go here
+});
+
+// `sequelize.define` also returns the model
+console.log(AccountModel === sequelize.models.AccountModel); // true
