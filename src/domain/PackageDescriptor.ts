714c61abf2010896cdf5712a9b054f0795dfed0e
Jake O'Loughlin <c3350038@uon.edu.au>
2022-08-15 20:05:02 +1000

    Added explore and package domain classes

---
+++ src/domain/PackageDescriptor.ts
@@ -0,0 +1,28 @@
+class PackageDescriptor{
+    descriptorId: number;
+    packageId: number;
+
+    constructor(
+        descriptorId: number,
+        packageId: number
+    ) {
+        this.descriptorId = descriptorId;
+        this.packageId = packageId;
+    }
+    
+    // Getters
+    public getDescriptorId = (): number => {
+        return this.descriptorId;
+    }    
+    public getPackageId = (): number => {
+        return this.packageId;
+    }
+
+    // Setters
+    public setDescriptorId = (descriptorId: number) => {
+        this.descriptorId = descriptorId;
+    }
+    public setPackageId = (packageId: number) => {
+        this.packageId = packageId;
+    }
+}
