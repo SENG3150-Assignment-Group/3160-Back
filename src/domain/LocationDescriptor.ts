fe82dae2b503b8ddcb16432ecc2db708480eff13
Jake O'Loughlin <c3350038@uon.edu.au>
2022-08-15 20:06:26 +1000

    Added explore and package domain classes

---
+++ src/domain/LocationDescriptor.ts
@@ -0,0 +1,28 @@
+class LocationDescriptor{
+    descriptorId: number;
+    locationId: number;
+
+    constructor(
+        descriptorId: number,
+        locationId: number
+    ) {
+        this.descriptorId = descriptorId;
+        this.locationId = locationId;
+    }
+
+    // Getters
+    public getDescriptorId = (): number => {
+        return this.descriptorId;
+    }
+    public getLocationId = (): number => {
+        return this.locationId;
+    }
+
+    // Setters
+    public setDescriptorId = (descriptorId: number) => {
+        this.descriptorId = descriptorId;
+    }
+    public setLocationId = (locationId: number) => {
+        this.locationId = locationId;
+    }
+}
