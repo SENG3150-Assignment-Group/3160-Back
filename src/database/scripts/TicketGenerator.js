404016f3f4ab269ee0a7b182cab9d26ce8b4e93d
anton <antoine.lyamin@gmail.com>
2022-08-23 12:04:51 +1000

    added execution path as a comment

--- src/database/scripts/TicketGenerator.js
+++ src/database/scripts/TicketGenerator.js
@@ -1,11 +1,10 @@
-const planes = {
-  747: { seats: 500, firstClass: 50, economy: 450 },
-  a380: { seats: 500, firstClass: 50, economy: 450 },
-};
+// const planes = {
+//   747: { seats: 500, firstClass: 50, economy: 450 },
+//   a380: { seats: 500, firstClass: 50, economy: 450 },
+// };
 
-
-const ticketCode;
+// const ticketCode;
 
 console.log(
-  `INSERT INTO Tickets (TicketCode, TicketClass, Price, Booked, Transferable, Exchangable, Refundable, AccountId, PersonType, SpecialRequests, DietaryPreferences, CarryOnBaggage, CheckedBaggage, FlightId, BookingId) VALUES (${ticketCode});`
+  `INSERT INTO Tickets (TicketCode, TicketClass, Price, Booked, Transferable, Exchangable, Refundable, AccountId, PersonType, SpecialRequests, DietaryPreferences, CarryOnBaggage, CheckedBaggage, FlightId, BookingId) VALUES ();`
 );
